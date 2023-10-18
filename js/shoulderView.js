42980a9ffe541f60af54563c3da7969594ad861d
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-10-18 11:29:55 +0200

    La til kroppsdeler og shoulderView
    Bør diskuteres før mer gjøres

---
+++ js/shoulderView.js
@@ -0,0 +1,12 @@
+function shoulderView(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+    <h1>MuscleWiki</h1>
+
+    <div>Informasjon om kroppsdel og treningsforslag</div>
+    <div> videolink </div>
+
+
+    <!-- placeholder bilde -->
+    <img class="wikiShoulder"src="https://www.shoulder-pain-explained.com/images/shoulder-bones-anatomy.png"><img>
+    `;
+}
--- js/wikiView.js
+++ js/wikiView.js
@@ -1,6 +1,16 @@
 function wikiPage(){
     document.getElementById('app').innerHTML = /*HTML*/`
     <h1>MuscleWiki</h1>
-    <img class="wikiBody"src="https://www.researchgate.net/profile/Yossi-Nagar/publication/352817535/figure/fig3/AS:1040001202675714@1624967017421/Completeness-diagram-of-the-skeleton-Drawing-by-Y-Nagar.ppm"<img>
+
+    <!-- disse skal posisjoneres og være usynlige -->
+    <div class="shoulder"id="shoulderDiv" onclick="shoulderView()">shoulder</div>
+    <div class="arm"id="armDiv">arm</div>
+    <div class="chest"id="chestDiv">chest</div>
+    <div class="stomach"id="stomachDiv">stomach</div>
+    <div class="back"id="backDiv">back</div>
+    <div class="leg"id="legDiv">leg</div>
+
+    <!-- placeholder bilde -->
+    <img class="wikiBody"src="https://www.researchgate.net/profile/Yossi-Nagar/publication/352817535/figure/fig3/AS:1040001202675714@1624967017421/Completeness-diagram-of-the-skeleton-Drawing-by-Y-Nagar.ppm"><img>
     `;
 }
