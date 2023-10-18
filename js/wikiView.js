14ff5032ff6477a2b605ece3e83f05bd231f384f
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-10-18 10:23:09 +0200

    Added wikiView

---
+++ js/wikiView.js
@@ -0,0 +1,6 @@
+function wikiPage(){
+    document.getElementById('app').innerHTML = /*HTML*/`
+    <h1>MuscleWiki</h1>
+    <img class="wikiBody"src="https://www.researchgate.net/profile/Yossi-Nagar/publication/352817535/figure/fig3/AS:1040001202675714@1624967017421/Completeness-diagram-of-the-skeleton-Drawing-by-Y-Nagar.ppm"<img>
+    `;
+}
