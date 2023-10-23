135edd6a0230f56a95e565351d26196f530af2d1
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-10-23 14:26:47 +0200

    Beskrivelser i wikiView

--- js/wikiView.js
+++ js/wikiView.js
@@ -1,11 +1,14 @@
 function wikiPage(){
     document.getElementById('app').innerHTML = /*HTML*/`
 
+    <!-- knapp for å gå tilbake til mainPage -->
     <button onclick="mainPage()">X</button>
     <span>Main page</span>
 
     <div class="image-container">
     <h1>MuscleWiki</h1>
+
+    <!-- usynlige div'er for å trykke på deler av kroppen -->
     <img class="wikiBody"src="https://www.researchgate.net/profile/Yossi-Nagar/publication/352817535/figure/fig3/AS:1040001202675714@1624967017421/Completeness-diagram-of-the-skeleton-Drawing-by-Y-Nagar.ppm"><img>
     <div class="leftShoulder"id="shoulderDiv" onclick="shoulderView()">shoulder</div>
     <div class="rightShoulder"id="shoulderDiv" onclick="shoulderView()">shoulder</div>
@@ -18,6 +21,7 @@ function wikiPage(){
     <div class="leftLeg"id="legDiv"onclick="legView()">leg</div>
     <div class="rightLeg"id="legDiv"onclick="legView()">leg</div>
     </div>
-
     `;
 }
+
+
