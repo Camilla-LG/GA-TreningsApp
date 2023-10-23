14c0a14ce1b8991a037ff9535e08138942bc8024
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-10-23 11:16:50 +0200

    La til en image-container for å posisjonere diver i wikiView

--- js/wikiView.js
+++ js/wikiView.js
@@ -1,16 +1,22 @@
 function wikiPage(){
     document.getElementById('app').innerHTML = /*HTML*/`
-    <h1>MuscleWiki</h1>
 
-    <!-- disse skal posisjoneres og være usynlige -->
-    <div class="shoulder"id="shoulderDiv" onclick="shoulderView()">shoulder</div>
-    <div class="arm"id="armDiv" onclick="armView()">arm</div>
+    <button onclick="mainPage()">X</button>
+    <span>Main page</span>
+
+    <div class="image-container">
+    <h1>MuscleWiki</h1>
+    <img class="wikiBody"src="https://www.researchgate.net/profile/Yossi-Nagar/publication/352817535/figure/fig3/AS:1040001202675714@1624967017421/Completeness-diagram-of-the-skeleton-Drawing-by-Y-Nagar.ppm"><img>
+    <div class="leftShoulder"id="shoulderDiv" onclick="shoulderView()">shoulder</div>
+    <div class="rightShoulder"id="shoulderDiv" onclick="shoulderView()">shoulder</div>
+    <div class="leftArm"id="armDiv" onclick="armView()">arm</div>
+    <div class="rightArm"id="armDiv" onclick="armView()">arm</div>
     <div class="chest"id="chestDiv" onclick="chestView()">chest</div>
     <div class="stomach"id="stomachDiv"onclick="stomachView()">stomach</div>
     <div class="knee"id="backDiv"onclick="kneeView()">knee</div>
     <div class="leg"id="legDiv"onclick="legView()">leg</div>
+    </div>
 
-    <!-- placeholder bilde -->
-    <img class="wikiBody"src="https://www.researchgate.net/profile/Yossi-Nagar/publication/352817535/figure/fig3/AS:1040001202675714@1624967017421/Completeness-diagram-of-the-skeleton-Drawing-by-Y-Nagar.ppm"><img>
     `;
 }
+
