aa11e60d69390e2e0c60368ca099b4b908cc76f3
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-10-24 14:46:15 +0200

    La til videovisning i shoulderView og posisjonerte i css

--- js/shoulderView.js
+++ js/shoulderView.js
@@ -1,14 +1,18 @@
 function shoulderView(){
     document.getElementById('app').innerHTML = /*HTML*/`
+
+    <div class="image-containerWiki">
     <h1>MuscleWiki</h1>
+    <button class="wikiMenuButton"onclick="wikiPage()">X</button>
+    <img class="wikiShoulder" src="https://www.shoulder-pain-explained.com/images/shoulder-bones-anatomy.png"><img>
+    <div class="treningsInfo">Informasjon om skulder og treningsforslag</div>
+    
+    <iframe class="videoDisplay" width="632" height="355.5" src="https://www.youtube.com/embed/cBSD6mQIPQk" title="Dumbbell Bicep Curl | Nuffield Health" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
-    <button onclick="wikiPage()">X</button>
+    <div class="prevArrow" id="prevArrow"></div>
+    <div class="nextArrow" id="nextArrow"></div>
 
-    <div>Informasjon om skulder og treningsforslag</div>
-    <div>videolink</div>
-
-
-    <!-- placeholder bilde -->
-    <img class="wikiShoulder"src="https://www.shoulder-pain-explained.com/images/shoulder-bones-anatomy.png"><img>
+    </div>
     `;
 }
+
