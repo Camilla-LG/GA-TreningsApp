dad10bb1a1e49c3287121ecbda2befaedcdb42f5
Camilla-LG <116033196+Camilla-LG@users.noreply.github.com>
2023-10-25 13:23:05 +0200

    Updated css
    Updated css for settingsView.js, just basic layout with placement of items. No special design

--- js/settingsView.js
+++ js/settingsView.js
@@ -2,54 +2,58 @@ function settingsPage() {
   document.getElementById("app").innerHTML = /*HTML*/ `
 
 <h1> Settings </h1>
-<div onclick="mainPage()"> Main Page </div>
 
-<div class="options">
-  <div class="type">
-    <a> Mode </a>
-    <a> Units </a>
-    <a> Dato visning </a>
-    <a> Tid visning </a>
-    <a> Påminnelse </a>
-    <a> Tilbakemeldinger </a>
+<div class="settingsArea">
+  <div class="options">
+    <div class="type">
+      <a> Mode </a>
+      <a> Units </a>
+      <a> Dato visning </a>
+      <a> Tid visning </a>
+      <a> Påminnelse </a>
+      <a> Tilbakemeldinger </a>
+    </div>
+    <div class="dropdowns">
+      <select class="mode" name="modeSelect" onchange="selectModeOption()">
+        <option> Light </option>
+        <option> Dark </option>
+      </select>
+      <select id="unit" name="unitSelect" onchange="selectUnitOption()">
+        <option value= "metric"> Metric </option>
+        <option value="imperial"> Imperial </option>
+      </select>
+      <select id="dateFormat" name="dateSelect"  onchange="selectDateOption()">
+        <option value="numeric"> DD/MM/YYYY </option>
+        <option value="text"> Day, Month, Year </option>  
+      </select>
+      <select id="timeFormat" name="timeSelect" onchange="selectTimeOption()">
+      <option value="12hr"> AM/PM </option>
+      <option value="24hr"> 24 Timer </option>
+      </select>
+      <select id="reminder" name="reminderSelect" onchange="selectReminder()">
+      <option> Ja </option>
+      <option> Nei </option>
+      </select>
+      <select id="feedback" name="feedbackSelect" onchange="selectFeedback()">
+      <option> Ja </option>
+      <option> Nei </option>
+      </select>
+    </div>
   </div>
-  <div class="dropdowns">
-    <select class="mode" name="modeSelect" onchange="selectModeOption()">
-      <option> Light </option>
-      <option> Dark </option>
-    </select>
-    <select id="unit" name="unitSelect" onchange="selectUnitOption()">
-      <option value="metric"> Metric </option>
-      <option value="imperial"> Imperial </option>
-    </select>
-    <select id="dateFormat" name="dateSelect"  onchange="selectDateOption()">
-      <option value="numeric"> DD/MM/YYYY </option>
-      <option value="text"> Day, Month, Year </option>  
-    </select>
-    <select id="timeFormat" name="timeSelect" onchange="selectTimeOption()">
-    <option value="12hr"> AM/PM </option>
-    <option value="24hr"> 24 Timer </option>
-    </select>
-    <select id="reminder" name="reminderSelect" onchange="selectReminder()">
-    <option> Ja </option>
-    <option> Nei </option>
-    </select>
-    <select id="feedback" name="feedbackSelect" onchange="selectFeedback()">
-    <option> Ja </option>
-    <option> Nei </option>
-    </select>
-    </div>
-    </div>
-    
-    
+  
+  <div class="optionButtons">
     <button onclick="saveSettings()"> Lagre Innstillinger </button>
     <button onclick="editPassword()">Edit password</button>
     <button onclick="mainPage()">Main page</button>
-    
+  </div>
+  
+  <div class="testOutputs">
     <div class='outputUnit' id="height"></div>
     <div class='outputUnit' id="weight"></div>
     <div id='outputDate'></div>
     <div id='outputTime'></div>
+  </div>
+</div>
     `;
 }
 
