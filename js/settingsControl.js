a1c457a5647e0e490a56b4d7b2a56a5ef266f3b3
Camilla-LG <116033196+Camilla-LG@users.noreply.github.com>
2023-10-25 13:58:37 +0200

    errors resolved
    Resovled errors with clock not going continuosly (it now works), removed link to non-exisiting js.file in index, removed eventListener in settingsfunctions as they where not needed

--- js/settingsControl.js
+++ js/settingsControl.js
@@ -26,9 +26,7 @@ function selectModeOption(){
 }
 
 
-//Med denne funksjonen, velger du mellom metric og imperial enhetssystem, det er 
-//lagt til eventListener utenfor funksjonen slik at den alltid sjekker om
-//man endrer valg i dropdown menyen
+//Med denne funksjonen, velger du mellom metric og imperial enhetssystem
 
 function selectUnitOption() {
   let unitFormat = document.getElementById("unit");
@@ -44,12 +42,10 @@ function selectUnitOption() {
       height.textContent = "Ft";
     }
 }
-document.getElementById("unit").addEventListener("change", selectUnitOption);
 
 
-//Med denne funksjonen, velger du hvordan du ønsker at datoen skal vises, det er 
-//lagt til eventListener utenfor funksjonen slik at den alltid sjekker om
-//man endrer valg i dropdown menyen
+
+//Med denne funksjonen, velger du hvordan du ønsker at datoen skal vises
 
 function selectDateOption(){
   let dateFormat = document.getElementById("dateFormat");
@@ -65,12 +61,10 @@ function selectDateOption(){
   }
   document.getElementById('outputDate').textContent = chosenDateFormat;
 }
-document.getElementById("dateFormat").addEventListener("change", selectDateOption);
 
 
-//Med denne funksjonen, velger du hvordan du ønsker at klokken skal vises, det er 
-//lagt til eventListener utenfor funksjonen slik at den alltid sjekker om
-//man endrer valg i dropdown menyen
+
+//Med denne funksjonen, velger du hvordan du ønsker at klokken skal vises
 
 function selectTimeOption(){
   let timeFormat = document.getElementById("timeFormat");
@@ -84,6 +78,5 @@ function selectTimeOption(){
     chosenTimeFormat = currentTime.toLocaleString('nb-NO', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false});
   }
   document.getElementById('outputTime').textContent = chosenTimeFormat;
+  setInterval(selectTimeOption, 1000);
 }
-document.getElementById('timeFormat').addEventListener("change", selectTimeOption);
-selectTimeOption();
