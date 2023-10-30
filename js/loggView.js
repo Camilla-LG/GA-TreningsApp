e686f3971492025946eaebf9f00336c70b9f2d02
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-10-30 10:18:59 +0100

    x

--- js/loggView.js
+++ js/loggView.js
@@ -1,35 +1,23 @@
 // Legg view for logg her Said
 
-
-
-
-
-
-
-
-
-
-
-
-
-// function loggView(){
-//     let loggHtml = /*HTML*/`
-//     <h1>Choose week</h1>
-//     <div style="witdh: fit-content; overflow: auto;">
-//         ${drawLoggInv()}
-//     <div>
+function loggView(){
+    let loggHtml = /*HTML*/`
+    <h1>Choose week</h1>
+    <div style="witdh: fit-content; overflow: auto;">
+        ${drawLoggInv()}
+    <div>
     
-//     `;
-//     document.getElementById('app').innerHTML = loggHtml
-// }
+    `;
+    document.getElementById('app').innerHTML = loggHtml
+}
 
 
-// function drawLoggInv() {
-//     let weeks = 1
-//     let ave = ``
-//     for(let week of model.app.loggedInUser.log){
-//        ave += /*HTML*/ `<button onclick="model.app.loggedInUser.currentWeek = ${weeks}">Week ${weeks}</button>`
-//        weeks++
-//     }
-//     return ave;
-// }
+function drawLoggInv() {
+    let weeks = 1
+    let ave = ``
+    for(let week of model.app.loggedInUser.log){
+       ave += /*HTML*/ `<button onclick="model.app.loggedInUser.currentWeek = ${weeks}">Week ${weeks}</button>`
+       weeks++
+    }
+    return ave;
+}
