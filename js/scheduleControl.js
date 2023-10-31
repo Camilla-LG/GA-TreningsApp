b18d8ab6aab79d577dbdc6bf593a33b72b7b634a
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-10-31 11:06:06 +0100

    x

--- js/scheduleControl.js
+++ js/scheduleControl.js
@@ -1,6 +1,16 @@
 function finishDay() {
+    const currentDate = new Date(); // Current date and time
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1; // Months are zero-based
+    const year = currentDate.getFullYear();
+  
+    // Push the log entry with the custom date and tasks
     model.app.loggedInUser.log.push({
-        dato: newDate(),
-        task: tasks
-    })
-}
+      dato: `${year}-${month}-${day}`,
+      task: tasks
+    });
+    tasks = [];
+    task = {};
+    updateTaskList()
+  }
+  
