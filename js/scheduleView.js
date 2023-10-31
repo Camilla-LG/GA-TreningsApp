03800b41734910ef7fdc7d7523b51104d7d7ccd5
Sneip1 <141616568+Sneip1@users.noreply.github.com>
2023-10-31 09:34:55 +0100

    s

--- js/scheduleView.js
+++ js/scheduleView.js
@@ -5,52 +5,65 @@ function scheduleView() {
         <h2>Dagens timeplan</h2>
         <table>
             <tr>
+                <th>Dato</th>
                 <th>Tidspunkt</th>
                 <th>Aktivitet</th>
+                <th>Antall sett</th>
             </tr>
         </table>
 
-        <h2>Legg til oppgave</h2>
-        <input type="text" id="taskInput" placeholder="Skriv inn oppgave">
+        <h2>Legg til økt</h2>
+        <input type="date" id="taskDate"> <!-- Legg til dato-input -->
+        <input type="text" id="taskInput" placeholder="Skriv inn øvelse">
+        <input type="number" id="setCount" placeholder="Antall sett">
         <input type="time" id="taskTime"> <!-- Legg til tidspunkt-input -->
-        <button onclick="addTask()">Legg til oppgave</button>
+        <button onclick="addTask()">Legg til økt</button>
         <ul id="taskList"></ul>
     `;
 }
 
 function addTask() {
+    const taskDate = document.getElementById('taskDate');
     const taskInput = document.getElementById('taskInput');
+    const setCount = document.getElementById('setCount');
     const taskTime = document.getElementById('taskTime');
     const taskList = document.getElementById('taskList');
 
-    if (taskInput.value.trim() === '' || taskTime.value === '') {
-        alert('Oppgaven og tidspunktet kan ikke være tomme');
+    if (taskInput.value.trim() === '' || taskTime.value === '' || taskDate.value === '' || setCount.value === '') {
+        alert('Øvelse, dato, tidspunkt og antall sett kan ikke være tomme');
         return;
     }
 
     const task = {
-        text: taskInput.value,
+        date: taskDate.value,
+        exercise: taskInput.value,
+        sets: parseInt(setCount.value),
         time: taskTime.value,
     };
 
     tasks.push(task);
+    taskDate.value = '';
     taskInput.value = '';
+    setCount.value = '';
     taskTime.value = '';
     updateTaskList();
 }
 
 function updateTaskList() {
     const taskTable = document.querySelector('table');
-    
-    tasks.forEach(task => {
-        const newRow = taskTable.insertRow(-1); 
-        const timeCell = newRow.insertCell(0);
-        const activityCell = newRow.insertCell(1);
 
+    tasks.forEach(task => {
+        const newRow = taskTable.insertRow(-1);
+        const dateCell = newRow.insertCell(0);
+        const timeCell = newRow.insertCell(1);
+        const exerciseCell = newRow.insertCell(2);
+        const setsCell = newRow.insertCell(3);
+
+        dateCell.textContent = task.date;
         timeCell.textContent = task.time;
-        activityCell.textContent = task.text;
+        exerciseCell.textContent = task.exercise;
+        setsCell.textContent = task.sets;
     });
 }
 
 // scheduleView();
-
