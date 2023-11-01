cf3881fd4fc2333cff811c84593808af465ed39f
Blazej002 <141903573+Blazej002@users.noreply.github.com>
2023-11-01 11:23:55 +0100

    tweaks

--- js/scheduleControl.js
+++ js/scheduleControl.js
@@ -1,18 +1,22 @@
 function finishDay(location) {
-    const currentDate = new Date(); // Current date and time
+    const currentDate = new Date();
     const day = currentDate.getDate();
-    const month = currentDate.getMonth() + 1; // Months are zero-based
+    const month = currentDate.getMonth() + 1; 
     const year = currentDate.getFullYear();
   
-    // Push the log entry with the custom date and tasks
-    model.app.loggedInUser.log.push({
-      dato: `${year}-${month}-${day}`,
-      task: model.input.schedule.task
-    });
-    model.input.schedule.task = [];
-    task = {};
-    if(location === 'main'){
-      mainPage()
-    }else updateTaskList();
+    if(model.input.schedule.task == ''){
+      alert('No workouts found')
+      return;
+    }else {
+      model.app.loggedInUser.log.push({
+        dato: `${year}-${month}-${day}`,
+        task: model.input.schedule.task
+      });
+      model.input.schedule.task = [];
+      task = {};
+      if(location === 'main'){
+        mainPage()
+      }else updateTaskList();
+    }
   }
   
