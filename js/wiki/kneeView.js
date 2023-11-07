1c52fb521d4e92382435ff5de69b8a1d30001ea1
Sneip1 <141616568+Sneip1@users.noreply.github.com>
2023-11-07 12:30:11 +0100

    stomach and knees

--- js/wiki/kneeView.js
+++ js/wiki/kneeView.js
@@ -7,7 +7,11 @@ function kneeView(){
     <button class="wikiHeaderBackground"></button>
     <button class="wikiMenuButton"onclick="wikiPage()">X</button>
    <!-- <img class="wikiShoulder" src="https://www.shoulder-pain-explained.com/images/shoulder-bones-anatomy.png"><img> -->
-    <div class="treningsInfo">Knees <p> Kneøvelse går her
+    <div class="treningsInfo">Knees <p> 
+    <li>Lunges: Ta et skritt fremover med en fot og senk hoften til det bakre kneet er nær bakken. Press deg opp igjen til startposisjonen. Gjenta på begge sider.</li>
+    <li>Leg Press: Denne øvelsen kan utføres på en maskin på treningssenteret. Du sitter og dytter vekter med føttene. Pass på å bruke passende vekter og teknikk for å unngå belastning på knærne.</li>
+    <li>Step-ups: Bruk en stepkasse eller lav benk. Trinn opp med en fot og presse deg opp til du står rett på boksen, og deretter senk deg ned igjen. Gjenta på begge sider.</li>
+ 
      </div>
     
     <iframe class="videoDisplay" id="videoId" width="632" height="355.5" src="https://www.youtube.com/embed/RzL-mqiQW8M" title="Exercises for Knee Pain Relief" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
--- js/wiki/stomachView.js
+++ js/wiki/stomachView.js
@@ -7,15 +7,18 @@ function stomachView(){
     <button class="wikiMenuButton"onclick="wikiPage()">X</button>
     
    <!-- <img class="wikiShoulder" src="https://www.shoulder-pain-explained.com/images/shoulder-bones-anatomy.png"><img> -->
-    <div class="treningsInfo">Stomach <p> Mageøvelse går her
+    <div class="treningsInfo">Stomach <p>   
+    <li>Crunches: Ligge på ryggen med bøyde knær og føttene flatt på bakken. Plasser hendene bak hodet eller over brystet og løft skuldrene fra gulvet ved å bruke magemusklene. Senk deg tilbake til startposisjonen uten å hvile helt på gulvet mellom gjentakelsene.</li>
+    <li>Beinhev: Ligg på ryggen og løft langsomt bena opp mot taket mens du holder dem rette. Senk dem deretter kontrollert tilbake til startposisjonen uten å la dem berøre gulvet.</li>
+   
      </div>
     
      <iframe class="videoDisplay" id="videoId" width="632" height="355.5" src="https://www.youtube.com/embed/RKqb61-zTyY" title="Best 7 ABS Exercises For SIX PACK - Gym Body Motivation" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
      <!-- Piler med funksjonalitet til å bytte video -->
      <div class="prevArrow" id="prevArrow" onclick="selectExcercise(this)" videoUrl="https://www.youtube.com/embed/cBSD6mQIPQk"></div>
-     <div class="nextArrow" id="nextArrow" onclick="selectExcercise(this)" videoUrl="https://www.youtube.com/embed/WZxUiocabAE"></div> 
-
+     <div class="nextArrow" id="nextArrow" onclick="selectExcercise(this)" videoUrl="https://www.youtube.com/embed/32JF-IT7BLk"></div> 
+    
     </div>
     `;
 }
