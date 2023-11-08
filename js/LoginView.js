561499761b9d9024c147538e9a6b348a5de649e9
Viktor Stokstad <141615732+Vikkidub@users.noreply.github.com>
2023-11-08 14:41:39 +0100

    loginView: logg in knappen kjører mainPage  direkte

--- js/LoginView.js
+++ js/LoginView.js
@@ -1,10 +1,10 @@
-
+loginView()
 function loginView(){
     document.getElementById('app').innerHTML = /*HTML*/`
     <h1>MuscleWiki</h1>
     <input type="text" placeholder="Username" onchange="model.input.loginAs.userName = this.value">
     <input type="password" placeholder="Password" onchange="model.input.loginAs.password = this.value"> 
-    <button onclick="loginControl()">Login</button>
+    <button onclick="mainPage()">Login</button>
     <button onclick="RegisterPage()">Register</button>
     `;
 }
